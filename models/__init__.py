70d9006318a2cde5469ace31fde535bf5e69a6a6
Germán Ponce Dominguez <german.ponce@argil.mx>
2024-06-20 18:16:41 -0600

    Actualizaciones

--- models/__init__.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-from . import l10n_mx_account_edi_download
-from . import account_move
-from . import account_move_line
--- views/__init__.py
+++
@@ -1,3 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import l10n_mx_edi_download
